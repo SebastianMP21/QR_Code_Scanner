d881be70a3908105eb03162109b0d9d5311c06c4
SebastianMP21 <sebastian.marquez@tecsup.edu.pe>
2022-10-13 22:47:05 -0500

    Agregar archivos de proyecto.

---
+++ QR_Code_Scanner/QR_Code_Scanner/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+
+
+using Xamarin.Forms.Xaml;
+
+namespace QR_Code_Scanner
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScannerQR_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = new ZXing.Mobile.MobileBarcodeScanner();
+                scanner.TopText = "Hola soy Sebastian Marquez \ny esto es un QR";
+                scanner.BottomText = "Semana 8";
+                var result = await scanner.Scan();
+                if (result != null)
+                {
+                    txtResultado.Text = result.Text;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert("Error", ex.Message.ToString(), "OK");
+            }
+        }
+    }
+}
